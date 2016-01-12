8e57da60a53917f09766c91611f1f494b0395393
MG <mg@mgs-macbook-air.local>
2016-01-12 14:07:52 -0800

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by MG on 1/12/16.
+//  Copyright © 2016 mobile makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by MG on 1/12/16.
+//  Copyright © 2016 mobile makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
