7d9507ca282d2217c5e7605c891ded8c6e561098
Steve Uffelman <sruffelman@gmail.com>
2016-01-12 15:18:12 -0800

    bottom bar created with back button

---
+++ MySafari/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MySafari
+//
+//  Created by Steve Uffelman on 1/12/16.
+//  Copyright © 2016 mobile makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
