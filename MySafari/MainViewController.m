e06f1c0a8505e1959733afcd2fe6390f3285ee28
MG <mg@MGs-MacBook-Air.local>
2016-01-12 17:26:56 -0800

    Mike's changes

--- MySafari/MainViewController.m
+++ MySafari/MainViewController.m
@@ -13,6 +13,8 @@
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityMonitor;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
 
 @end
 
@@ -22,6 +24,17 @@
     [super viewDidLoad];
     self.webView.delegate = self;
     self.urlTextField.delegate = self;
+    if ([self.webView canGoBack] == YES) {
+        [self.backButton setEnabled:YES];
+    }else{
+        [self.backButton setEnabled:NO];
+    }
+    if ([self.webView canGoForward] == YES) {
+        [self.forwardButton setEnabled:YES];
+    }else{
+        [self.forwardButton setEnabled:NO];
+    }
+ 
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,8 +43,15 @@
 }
 
 -(BOOL) textFieldShouldReturn:(UITextField *)textField {
-    NSURL *url = [NSURL URLWithString:self.urlTextField.text];
-    [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+    if ([self.urlTextField.text hasPrefix:@"http://"]) {
+        NSURL *url = [NSURL URLWithString:self.urlTextField.text];
+        [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+    }else{
+        NSString *fixedURL = [NSString stringWithFormat:@"http://%@", self.urlTextField.text];
+        NSURL *url = [NSURL URLWithString:fixedURL];
+        [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+    }
+    
     return YES;
 }
 
@@ -40,10 +60,37 @@
 }
 
 -(void) webViewDidFinishLoad:(UIWebView *)webView {
+    if ([self.webView canGoBack] == YES) {
+        [self.backButton setEnabled:YES];
+    }else{
+        [self.backButton setEnabled:NO];
+    }
     [self.activityMonitor stopAnimating];
 }
 
 - (IBAction)onBackButtonPressed:(id)sender {
-    [self.webView goBack];
+    if ([self.webView canGoBack] == YES) {
+        [self.webView goBack];
+    }
+    
+}
+- (IBAction)onForwardButtonPressed:(id)sender {
+    if ([self.webView canGoForward] == YES) {
+        [self.forwardButton setEnabled:YES];
+    }else{
+        [self.forwardButton setEnabled:NO];
+    }
+}
+- (IBAction)onStopLoadingButtonPressed:(id)sender {
+    [self.webView stopLoading];
+}
+- (IBAction)reload:(id)sender {
+    [self.webView reload];
+}
+- (IBAction)onPlusButtonTapped:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Coming Soon" message:@"Feature to come in future update" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okay = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:okay];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 @end
